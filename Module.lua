e389e789ea98bfeae80644a66e4fdf76d7035697
rbxexploiter <88555130+rbxexploiter@users.noreply.github.com>
2022-03-23 22:58:34 +0300

    Update Module.lua

--- Module.lua
+++ Module.lua
@@ -198,8 +198,6 @@ end
 local UserInputService = game:GetService("UserInputService")
 local runService = (game:GetService("RunService"));
 
-local gui = _G.Frame
-
 local dragging
 local dragInput
 local dragStart
@@ -215,7 +213,7 @@ local DRAG_SPEED = (8); -- // The speed of the UI darg.
 function Update(dt)
 	if not (startPos) then return end;
 	if not (dragging) and (lastGoalPos) then
-		gui.Position = UDim2.new(startPos.X.Scale, Lerp(gui.Position.X.Offset, lastGoalPos.X.Offset, dt * DRAG_SPEED), startPos.Y.Scale, Lerp(gui.Position.Y.Offset, lastGoalPos.Y.Offset, dt * DRAG_SPEED))
+		_G.Frame.Position = UDim2.new(startPos.X.Scale, Lerp(_G.Frame.Position.X.Offset, lastGoalPos.X.Offset, dt * DRAG_SPEED), startPos.Y.Scale, Lerp(_G.Frame.Position.Y.Offset, lastGoalPos.Y.Offset, dt * DRAG_SPEED))
 		return 
 	end;
 
@@ -223,14 +221,14 @@ function Update(dt)
 	local xGoal = (startPos.X.Offset - delta.X);
 	local yGoal = (startPos.Y.Offset - delta.Y);
 	lastGoalPos = UDim2.new(startPos.X.Scale, xGoal, startPos.Y.Scale, yGoal)
-	gui.Position = UDim2.new(startPos.X.Scale, Lerp(gui.Position.X.Offset, xGoal, dt * DRAG_SPEED), startPos.Y.Scale, Lerp(gui.Position.Y.Offset, yGoal, dt * DRAG_SPEED))
+	_G.Frame.Position = UDim2.new(startPos.X.Scale, Lerp(_G.Frame.Position.X.Offset, xGoal, dt * DRAG_SPEED), startPos.Y.Scale, Lerp(_G.Frame.Position.Y.Offset, yGoal, dt * DRAG_SPEED))
 end;
 
-gui.InputBegan:Connect(function(input)
+_G.Frame.InputBegan:Connect(function(input)
 	if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
 		dragging = true
 		dragStart = input.Position
-		startPos = gui.Position
+		startPos = _G.Frame.Position
 		lastMousePos = UserInputService:GetMouseLocation()
 
 		input.Changed:Connect(function()
@@ -241,7 +239,7 @@ gui.InputBegan:Connect(function(input)
 	end
 end)
 
-gui.InputChanged:Connect(function(input)
+_G.Frame.InputChanged:Connect(function(input)
 	if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
 		dragInput = input
 	end
